7b7eeaf08c098c01a2c1da1cffa131648fceaaa0
ilhom4177 <ilhommamatqulov276@gmail.com>
2022-12-17 14:21:41 +0500

    file06.py

--- file06.py
+++ file06.py
@@ -6,17 +6,5 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    n = 0
-    m = 0
-    l = []
-    for i in range(len(data)):
-        if data[i].redline()
-            m+=1
-        else:
-            n+=1
-    return l
-
-f = open("txt_file/data06.txt")
-a = f.read()
-print(main(a))
+   fgnf
 # Read data from file
