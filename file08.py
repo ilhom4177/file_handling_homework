aeafa90d06ee659e09b9e36b592b6f15b48c1be3
ilhom4177 <ilhommamatqulov276@gmail.com>
2022-12-17 12:50:37 +0500

    file08.py

--- file08.py
+++ file08.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    mx=0
+    for i in range(len(data)):
+        if data[i].isdigit():
+            if mx<int(data[i]):
+                mx=int(data[i])
+    return mx
+f=open('txt_file/data08.txt')
+a=f.read()
+print(main(a))
 # Read data from file
